38b41b3aa53f894f4b37e1bd9ffd73834e85764f
Miika Heini <32241374+miikaheini@users.noreply.github.com>
2018-07-03 19:53:38 +0000

    fix chart, add time frame selection

--- public/js/show.js
+++ public/js/show.js
@@ -1,3 +1,9 @@
+// Select elements for functions and set variables
+var timeMin = 0;
+var data = parseChartData(observations, tz);
+var ctx = document.getElementById("myChart").getContext("2d");
+var timeFrames = document.querySelectorAll(".frame");
+
 // Create a clock and input timezone as parameter
 function GetClock(tz) {
 	var time = moment().tz(tz).format("HH:mm:ss");
@@ -6,21 +12,19 @@ function GetClock(tz) {
 }
 
 // Parse data for chart
-function parseChartData() {
-	var data = [ 
-	{t: moment(), y: 10},
-	{t: moment().add(12, "hour"), y: 12},
-	{t: moment().add(1, "day"), y: 8},
-	{t: moment().add(36, "hour"), y: 19},
-	{t: moment().add(2, "day"), y: 15}
-	];
+function parseChartData(observations, tz) {
+	var data = [];
+	var limit = 0;
+
+	for( var i = 0; i < observations.length; i++){
+		data.push({t: moment(observations[i].t).tz(tz).format("DD.MM.YYYY, HH:mm"), y: observations[i].y});
+	};
 
 	return data;
 }
 
 // Draw temperature chart
-function drawChart(data, city) {
-	var ctx = document.getElementById("myChart").getContext("2d");
+function drawChart(ctx, data, city, timeFrame) {
 	var myChart = new Chart(ctx, {
 		type: "line",
 		data: {
@@ -45,20 +49,22 @@ function drawChart(data, city) {
 			scales: {
 				xAxes: [{
 					type: "time",
-					display: true,
-						scaleLabel: {
-							display: true,
-							labelString: "Date"
-						},
-						ticks: {
-							major: {
-								fontStyle: "bold",
-								fontColor: "#FF0000"
-							}
-						},
 					time: {
-						unit: "day"
-					}
+						min: timeFrame,
+						unit: "day",
+						parser: "DD.MM.YYYY, HH:mm"
+					},
+					display: true,
+					scaleLabel: {
+						display: true,
+						labelString: "Date"
+					},
+					ticks: {
+						major: {
+							fontStyle: "bold",
+							fontColor: "#FF0000"
+						}
+					},
 				}],
 				yAxes: [{
 					display: true,
@@ -76,10 +82,38 @@ function drawChart(data, city) {
 	});
 }
 
+// Add event listeners to time selection buttons
+function initialiseButtons(){
+	for (var i = 0; i < timeFrames.length; i++){
+		// Add click listeners to time frame buttons
+		timeFrames[i].addEventListener("click", function(){
+			// Remove highlighting from time frame buttons
+			for (var j = 0; j < timeFrames.length; j++){
+				timeFrames[j].classList.remove("primary");
+			}
+			// Add highlighting to selected time frame
+			this.classList.add("primary");
+			// Grab text from selected time frame button
+			var frame = this.textContent;
+			// Compare color to pickedColor
+			if (frame === "3 days"){
+				timeMin = moment().add(-3, "day");
+			} else if (frame === "Past week") {
+				timeMin = moment().add(-1, "week");
+			} else if (frame === "Past month") {
+				timeMin = moment().add(-1, "month");
+			} else {
+				timeMin = 0;
+			}
+			drawChart(ctx, data, city, timeMin);
+		});
+	}
+}
+
 // Load dynamic content on page load and set update intervals
 window.onload = function(){
-	// formatDates(tz);
 	GetClock(tz);
 	setInterval(function(){GetClock(tz)} , 1000);
-	drawChart(parseChartData(), city);
+	drawChart(ctx, data, city, timeMin);
+	initialiseButtons();
 }
